71c7b83f3849b684740abaa91ca95019f9672879
anasiangangster <yangaobo1996@gmail.com>
2020-07-21 14:26:23 +0800

    first commit

--- EventGenerator.java
+++ EventGenerator.java
@@ -6,8 +6,8 @@ import java.io.PrintWriter;
 class EventGenerator {
     public static void main(String[] args) {
 
-        String analyzerFilename = "../ResiGate/src/analyzer/protocol/mms/mm-analyzer-test.pac";
-        String eventFilename = "../ResiGate/src/analyzer/protocol/mms/event-test.bif";
+        String analyzerFilename = "../ResiGate/src/analyzer/protocol/mms/mms-analyzer-test.pac";
+        String eventFilename = "../ResiGate/src/analyzer/protocol/mms/event_test.bif";
 
         try {
             // write analyzer
@@ -17,15 +17,19 @@ class EventGenerator {
             analyzerOutputStream.println("refine flow MMS_Flow += {");
             analyzerOutputStream.println("  function rule_function(itemID: string, index: int):bool");
             analyzerOutputStream.println("  %{");
-            analyzerOutputStream.println("    // attr_value_string = map[{$itemID}][index]");
-            analyzerOutputStream.println("    // attr_value = cast_string_to_original_type();");
-            analyzerOutputStream.println("    // attr_value_string = map[{$itemID}][index]");
+            analyzerOutputStream.println("    // attr_value_string = map[{$itemID}][index];");
             analyzerOutputStream.println("    // attr_value = cast_string_to_original_type();");
             analyzerOutputStream.println("    // BifEvent::generate_mms_event_<i>(bro_analyzer(),");
             analyzerOutputStream.println("    //                                  bro_analyzer()->Conn(),");
             analyzerOutputStream.println("    //                                  attr_value_string,");
             analyzerOutputStream.println("    //                                  attr_value);");
-            analyzerOutputStream.println("    return true;");
+            analyzerOutputStream.println("    int attr_value_string = 1;");
+            analyzerOutputStream.println("    int attr_value = 1;");
+            analyzerOutputStream.println("    BifEvent::generate_mms_event_test(connection()->bro_analyzer(),");
+            analyzerOutputStream.println("                                      connection()->bro_analyzer()->Conn(),");
+            analyzerOutputStream.println("                                      attr_value_string,");
+            analyzerOutputStream.println("                                      attr_value);");
+            analyzerOutputStream.println("    return false;");
             analyzerOutputStream.println("  %}");
             analyzerOutputStream.println("}");
             analyzerOutputStream.close();
@@ -34,7 +38,12 @@ class EventGenerator {
             FileWriter eventWriter = new FileWriter(eventFilename);
             PrintWriter eventOutputStream = new PrintWriter(eventWriter);
 
-            eventOutputStream.println("event mms_event_test%(c: connection, itemID:string, res:bool%");
+            eventOutputStream.println("##");
+            eventOutputStream.println("##");
+            eventOutputStream.println("##");
+            eventOutputStream.println("##");
+            eventOutputStream.println("##");
+            eventOutputStream.println("event mms_event_test%(c: connection, itemID: int, res: int%);");
             eventOutputStream.close();
 
         } catch (FileNotFoundException e) {
